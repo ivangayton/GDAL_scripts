2f8a7a28a8379ef975a0171c1de04b085478881d
ivangayton <ivangayton@gmail.com>
2021-04-09 14:15:20 +0900

    stub of script for installing GDAL with ECW

---
+++ install_GDAL_with_ECW_on_Ubuntu_20-04.sh
@@ -0,0 +1,18 @@
+
+
+tar -xvf gdal-3.2.2.tar.gz
+
+./ECWJP2SDKSetup_5.5.0.2034.bin
+
+sudo cp -r hexagon/ERDAS-ECW_JPEG_2000_SDK-5.5.0/Desktop_Read-Only/ /usr/local
+
+sudo ln -s /usr/local/Desktop_Read-Only/lib/cpp11abi/x64/release/libNCSEcw.so.5.5.0 /usr/local/lib/libNCSEcw.so.5.5.0
+
+
+sudo apt install gcc
+sudo apt install g++
+sudo apt install libproj-dev
+
+cd gdal-3.2.2/
+
+./configure --with-ecw=/usr/local/Desktop_Read-Only
