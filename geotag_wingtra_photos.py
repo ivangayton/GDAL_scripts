8f25fdf3c3769d7c96acf01235676127677907f0
ivangayton <ivangayton@gmail.com>
2022-11-14 17:05:55 +0900

    possibly working utility to create ODM geo.txt file from Wingtra flight log and directory of photos

--- geotag_wingtra_photos.py
+++ geotag_wingtra_photos.py
@@ -12,7 +12,7 @@ def scandir(dir):
     filelist = []
     for path, dirs, files in os.walk(dir):
         for f in files:
-            filelist.append(os.path.join(path, f))
+            filelist.append(f)
     return filelist
     
 def extract_locations(infile):
@@ -20,6 +20,7 @@ def extract_locations(infile):
     contents = json.load(open(infile))
     track = contents['flights'][0]['geotag']
     csv = []
+    geotxt = []
     for point in track:
         coord = point['coordinate']
         lat = coord[0]
@@ -28,46 +29,63 @@ def extract_locations(infile):
         timestamp = point['timestamp']
         hacc = point['hAccuracy']
         vacc = point['vAccuracy']
-        
         pitch = point['pitch']
         roll = point['roll']
         yaw = point['yaw']
         sequence = point['sequence']
         version = point['version']
-
-        csv.append([sequence,lat,lon,ele,timestamp,hacc,
-                    vacc,pitch,roll,yaw,version])
-    return csv       
+        
+        csv.append([sequence,lat,lon,ele,timestamp,
+                    hacc,vacc,pitch,roll,yaw,version])
+        # for now ignoring the pitch, roll, and yaw until
+        # I understand how they work
+        geotxt.append([lon,lat,ele,0,0,0,hacc,vacc])
+        
+    return (csv,geotxt)       
     
-def create_geotag_list(indir):
-    """Create a CSV file with a list of photos and their lat & long"""
-    outfile = indir + '.csv'
+def images(indir):
+    """Get a list of images that hopefully corresponds to the locations in the flight log"""
     image_files = scandir(indir)
-    writer = csv.writer(open(outfile, 'w'), delimiter = ',')
-    writer.writerow(['file', 'path', 'directory', 'lat', 'lon'])
-    
-    for image_file in image_files:
-        (image_path, image_ext) = os.path.splitext(image_file)
-        image_filename = os.path.basename(image_file)
-        image_dirname = os.path.dirname(image_file)
-        if(image_ext == '.JPG' or image_ext == '.jpg'):
-            crds = extract_location(image_file)
-            if(crds):
-                writer.writerow([image_filename, image_file, image_dirname,
-                                 crds[0], crds[1]]) 
     
     
 if __name__ == "__main__":
     """Expects a json file with flight data and a directory of images"""
     flight_log_file = sys.argv[1]
     image_directory = sys.argv[2]
-    locations = extract_locations(flight_log_file)
+    (locations,geotxt) = extract_locations(flight_log_file)
+    locations.sort(key = lambda x: int(x[0])) #sort by sequence, maybe should be timestamp
+    image_files = scandir(image_directory)
+    image_files.sort() #alphabetize the fuckers
+
+    zipped = zip(image_files, locations)
+    merged_locations = []
+    for item in zipped:
+        newloc = []
+        newloc.append(item[0])
+        newloc.extend(item[1])
+        merged_locations.append(newloc)
+
+    zippedgeo = zip(image_files, geotxt)
+    merged_geo = []
+    for item in zippedgeo:
+        newloc = []
+        newloc.append(item[0])
+        newloc.extend(item[1])
+        merged_geo.append(newloc)
+    
     outcsv_file = os.path.splitext(flight_log_file)[0] + '.csv'
+    geotxt = os.path.splitext(flight_log_file)[0] + '.txt'
     with open(outcsv_file, 'w') as outf:
         writer = csv.writer(outf)
-        writer.writerow(['sequence','lat','lon','elevation','timestamp',
+        writer.writerow(['file','sequence','lat','lon','elevation','timestamp',
                          'hAccuracy','vAccuracy','pitch','roll','yaw','version'])
-        writer.writerows(locations)
+        writer.writerows(merged_locations)
+        
+    with open(geotxt, 'w') as geotxt_file:
+        writer = csv.writer(geotxt_file, delimiter = ' ')
+        writer.writerow(['EPSG:4326'])
+        writer.writerows(merged_geo)
+
 
 
     
