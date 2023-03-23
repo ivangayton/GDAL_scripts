27864c041563cc3e990678788e12d1dd47f773a0
ivangayton <ivangayton@gmail.com>
2023-03-23 10:49:09 +0900

    working oon Wingtra data

--- consolidate_odm_geotexts.py
+++ consolidate_odm_geotexts.py
@@ -11,17 +11,20 @@ file suitable for import into QGIS.
 
 The directory structure might look something like this:
 >my_flight_data/
-  >Mission/
-  |>odm_dem/
-    -dsm.tif
-    -dtm.tif
-  >odm_georeferencing/
-  |>odm_orthophoto/
-    -odm_orthophoto.tif
-  >odm_report/
-  >
-  -geo.txt
-  
+  >Mission1/
+    >flight1/
+      geo.txt
+      M01_F01_0001.JPG
+      M01_F01_0002.JPG
+      ...
+    >flight2
+      geo.txt
+      M01_F02_0001.JPG
+      M01_F02_0002.JPG
+      ...
+    ...
+  >Mission2/
+    ...
 """
 import sys, os
 import csv
@@ -38,17 +41,32 @@ def get_image_dirs(indir):
             dirlist.append(os.path.join(path, d))
     image_dirs = []
     for d in dirlist:
-        geotext = os.path.join(image_dir, 'geo.txt')
-        # TODO maybe also check if there are images
+        geotext = os.path.join(d, 'geo.txt')
+        # Also check if there are images, not essential
         if(os.path.isfile(geotext)):
-            odm_dirs.append(d)
+            image_dirs.append(d)
     return image_dirs
 
 def consolidate_geotexts(indir):
     image_dirs = get_image_dirs(indir)
-    for image_dir in image_dirs:
-        original = os.path.join(odm_dir, 'geo.txt')
-        basename = os.path.split(odm_dir)[-1]
+    with open(os.path.join(indir, 'geolocations.csv'), 'w') as locationsfile:
+        w = csv.writer(locationsfile)
+        header = ['filename', 'x','y','z','omega','phi','kappa',
+                  'hacc','vacc','block']
+        w.writerow(header)
+        for image_dir in image_dirs:
+            geotextfile = os.path.join(image_dir, 'geo.txt')
+            basename = os.path.split(os.path.split(image_dir)[0])[-1]
+            blockname = basename.replace(' ','_')
+            geotext = open(geotextfile, 'r')
+            locations = geotext.readlines()
+            crs = locations.pop(0) # mutates location removing first line
+            for location in locations:
+                row = location.split()
+                row.append(blockname)
+                w.writerow(row)
+            print(f'adding locations from {blockname}')
+        
         
 
 
@@ -64,6 +82,12 @@ if __name__ == '__main__':
     p.add_argument("indir",
                    help="Input directory full of ODM image dirs")
 
+    #TODO: implement a parameter to indicate how many directories deep the
+    # geo.txt file is from the shallower address.
+    p.add_argument("namedir", default = 2,
+                   help="number of nesting levels below the geo.txt file"
+                   "the name should be taken from")
+    
     a = p.parse_args()
 
     r = consolidate_geotexts(a.indir)
